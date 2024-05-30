10b7a4ebc485c615c0a1a5bec53b81ef115ec005
Pavlina <pavlina965@gmail.com>
2024-05-30 12:24:30 +0200

    about us, services

--- app/page.jsx
+++ app/page.jsx
@@ -17,11 +17,40 @@ export default function Home() {
           <span>
           <h1>Mikopa s.r.o</h1>
           <p>Zakázková výroba kuchyní, koupelen, ložnic, obývacích pokojů</p>
-          <p>Realizace kompletních interiérů kuchyní, koupelen, ložnic, obývacích pokojů</p>
           </span>
         </div>
       </section>
-      <section className={styles.more}>
+      <section >
+      <div className={styles.about}>
+        <img src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e'></img>
+        <div className={styles.container}>
+          <div className={styles.aboutText}>
+            <h1>Služby</h1>
+            <ul>
+            <li>Zakázková výroba kuchyní</li>
+            <li>Zakázková výroba koupelen</li>
+            <li>Zakázková výroba obývacích pokojů</li>
+            <li>Zakázková výroba ložnic</li>
+            </ul>
+        </div>
+        </div>
+      </div>
+      <div className={styles.about}>
+        <img src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e'></img>
+        <div className={styles.container}>
+          <div className={styles.aboutText}>
+            <h1>O nás</h1>
+            <h2>Mikopa</h2>
+            <p>Jsme česká firma zabývající se zakázovou výrobou kuchyní a veškerého interiérového nábytku z lamina,
+               foliovaných MDF desek a jiných moderních materiálů</p>
+            <p>Nabízíme realizaci kompletních interiérů kuchyní, koupelen, ložnic, obývacích pokojů,
+               včetně kompletní dodávky kancelářského nábytku a vybavení obchodů či jiných provozoven.
+                Naše služby obsahují jak dovoz tak i montáže.</p>
+          </div>
+        </div>
+      </div>
+      </section>
+      <section className={styles.images}>
         <ToggleButtons className={styles.button}/>
         <StandardImageList />
       </section>
