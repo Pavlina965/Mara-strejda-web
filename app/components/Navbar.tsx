6597257cbec912bca02a1c376253fbe0d9951448
Pavlina <pavlina965@gmail.com>
2024-06-23 13:23:16 +0200

    navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,123 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333", // Custom primary color
+    },
+    secondary: {
+      main: "#4caf50", // Custom secondary color
+    },
+  },
+});
+
+const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+      setIsOpen(open);
+    };
+
+  const menuItems = [
+    { text: "Home", href: "/" },
+    { text: "About", href: "/about" },
+    { text: "Services", href: "/services" },
+    { text: "Contact", href: "/contact" },
+  ];
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <AppBar
+          position="static"
+          sx={{ backgroundColor: theme.palette.primary.main }}
+        >
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Logo
+            </Typography>
+            <Box sx={{ display: { xs: "none", md: "block" } }}>
+              {menuItems.map((item) => (
+                <Button
+                  key={item.text}
+                  color="inherit"
+                  href={item.href}
+                  className={styles.listItem}
+                >
+                  {item.text}
+                </Button>
+              ))}
+            </Box>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ display: { xs: "block", md: "none" } }}
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          anchor="top"
+          open={isOpen}
+          onClose={toggleDrawer(false)}
+          sx={{
+            "& .MuiDrawer-paper": {
+              backgroundColor: theme.palette.primary.main,
+            },
+          }}
+        >
+          <Box
+            sx={{ width: "100%" }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+          >
+            <List>
+              {menuItems.map((item) => (
+                <ListItem
+                  button
+                  key={item.text}
+                  component="a"
+                  href={item.href}
+                  className={styles.drawerListItem}
+                >
+                  <ListItemText primary={item.text} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default Navbar;
--- app/page.jsx
+++ app/page.jsx
@@ -1,60 +1,61 @@
-// pages/index.js
-import React from 'react';
-import Head from 'next/head';
-import styles from './page.module.css';
-import ToggleButtons from './components/ToggleButton';
-import StandardImageList from './components/Pictures';
-import Navbar from './components/Navbar';
-
+import React from "react";
+import Head from "next/head";
+import styles from "./page.module.css";
+import ToggleButtons from "./components/ToggleButton";
+import StandardImageList from "./components/Pictures";
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <>
-    {/* <Navbar /> */}
-    
-    <div>
       <section className={styles.main}>
         <div className={styles.text}>
           <span>
-          <h1>Mikopa s.r.o</h1>
-          <p>Zakázková výroba kuchyní, koupelen, ložnic, obývacích pokojů</p>
+            <h1>Mikopa s.r.o</h1>
+            <p>Zakázková výroba kuchyní, koupelen, ložnic, obývacích pokojů</p>
           </span>
         </div>
       </section>
-      <section >
-      <div className={styles.about}>
-        <img src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e'></img>
-        <div className={styles.container}>
-          <div className={styles.aboutText}>
-            <h1>Služby</h1>
-            <ul>
-            <li>Zakázková výroba kuchyní</li>
-            <li>Zakázková výroba koupelen</li>
-            <li>Zakázková výroba obývacích pokojů</li>
-            <li>Zakázková výroba ložnic</li>
-            </ul>
-        </div>
-        </div>
-      </div>
-      <div className={styles.about}>
-        <img src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e'></img>
-        <div className={styles.container}>
-          <div className={styles.aboutText}>
-            <h1>O nás</h1>
-            <h2>Mikopa</h2>
-            <p>Jsme česká firma zabývající se zakázovou výrobou kuchyní a veškerého interiérového nábytku z lamina,
-               foliovaných MDF desek a jiných moderních materiálů</p>
-            <p>Nabízíme realizaci kompletních interiérů kuchyní, koupelen, ložnic, obývacích pokojů,
-               včetně kompletní dodávky kancelářského nábytku a vybavení obchodů či jiných provozoven.
-                Naše služby obsahují jak dovoz tak i montáže.</p>
+      <section>
+        <div className={styles.about}>
+          <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"></img>
+          <div className={styles.container}>
+            <div className={styles.aboutText}>
+              <h1>Služby</h1>
+              <ul>
+                <li>Zakázková výroba kuchyní</li>
+                <li>Zakázková výroba koupelen</li>
+                <li>Zakázková výroba obývacích pokojů</li>
+                <li>Zakázková výroba ložnic</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+
+        <div className={styles.about}>
+          <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"></img>
+          <div className={styles.container}>
+            <div className={styles.aboutText}>
+              <h1>O nás</h1>
+              <h2>Mikopa</h2>
+              <p>
+                Jsme česká firma zabývající se zakázovou výrobou kuchyní a
+                veškerého interiérového nábytku z lamina, foliovaných MDF desek
+                a jiných moderních materiálů
+              </p>
+              <p>
+                Nabízíme realizaci kompletních interiérů kuchyní, koupelen,
+                ložnic, obývacích pokojů, včetně kompletní dodávky kancelářského
+                nábytku a vybavení obchodů či jiných provozoven. Naše služby
+                obsahují jak dovoz tak i montáže.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
       <section className={styles.images}>
-        <ToggleButtons className={styles.button}/>
+        <ToggleButtons />
         <StandardImageList />
       </section>
-    </div>
     </>
   );
 }
