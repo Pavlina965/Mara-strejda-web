bce2a8418b33bd0110e891a85a15a29baf54e92c
Pavlina <pavlina965@gmail.com>
2024-06-24 16:41:09 +0200

    ThemeRegistryError

--- app/components/ThemeRegistry.tsx
+++ app/components/ThemeRegistry.tsx
@@ -1,47 +1,48 @@
 // components/ThemeRegistry.js
-'use client';
+"use client";
 
-import * as React from 'react';
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import * as React from "react";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#9a8a78',
+      main: "#9a8a78",
     },
     secondary: {
-      main: '#fff',
+      main: "#fff",
     },
     error: {
-      main: '#ff0000',
+      main: "#ff0000",
     },
     background: {
-      default: '#fff',
+      default: "#fff",
     },
   },
   // components: {
+  components: {
     MuiToggleButton: {
       styleOverrides: {
         root: {
-          backgroundColor: '#1976d2', // Primary main color
-          color: 'white',
-          '&.Mui-selected': {
-            backgroundColor: '#dc004e', // Secondary main color
-            color: 'white',
+          backgroundColor: "#1976d2", // Primary main color
+          color: "white",
+          "&.Mui-selected": {
+            backgroundColor: "#dc004e", // Secondary main color
+            color: "white",
           },
-          '&:hover': {
-            backgroundColor: '#115293', // Primary dark color
+          "&:hover": {
+            backgroundColor: "#115293", // Primary dark color
           },
         },
       },
     },
   },
-);
+});
 
-const cache = createCache({ key: 'css', prepend: true });
+const cache = createCache({ key: "css", prepend: true });
 
 export default function ThemeRegistry({ children }) {
   return (
